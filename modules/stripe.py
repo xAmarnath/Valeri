28351496d4bf48d03cc3ae769a2875334190e400
Amarnath C <amarnathcharichilil@gmail.com>
2022-06-29 21:21:27 +0530

    Update stripe.py

--- modules/stripe.py
+++ modules/stripe.py
@@ -31,11 +31,13 @@ def get_uuid(cc, exp, cvc):
     elif "authentication" in message:
         dcode = "3ds_vbv"
     elif "Your card has been declined" in dcode or "Your card was declined" in dcode:
-        message = dcode
         if message == 'N/A':
+           message = dcode
            dcode = 'generic_decline'
         else:
+           _message = dcode
            dcode = message
+           message = _message
     return resp["status"], dcode, message, resp["time"], emoji
 
 
