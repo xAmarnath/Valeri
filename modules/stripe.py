e0443c889c8c4beaadfe594988940e4541766d8b
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-06-29 15:28:37 +0000

    RoseLoverX: auto fixes

--- modules/stripe.py
+++ modules/stripe.py
@@ -11,24 +11,24 @@ def get_uuid(cc, exp, cvc):
     response = get(url, timeout=16)
     resp = response.json()
     dcode = resp["dcode"]
-    message = resp['message']
-    emoji = '❌'
-    if 'insufficient' in dcode:
-       message= dcode
-       dcode = 'insufficient_funds'
-       emoji = '✅'
-    elif 'security code' in dcode:
-       message = dcode
-       dcode = 'incorrect_cvc'
-       emoji = '✅'
-    elif 'card number is invalid' in dcode:
-       message = dcode
-       dcode = 'invalid_card'
-    elif 'does not support' in dcode:
-       message= dcode
-       dcode='card_not_support'
-    elif 'authentication' in message:
-       dcode = '3ds_vbv'
+    message = resp["message"]
+    emoji = "❌"
+    if "insufficient" in dcode:
+        message = dcode
+        dcode = "insufficient_funds"
+        emoji = "✅"
+    elif "security code" in dcode:
+        message = dcode
+        dcode = "incorrect_cvc"
+        emoji = "✅"
+    elif "card number is invalid" in dcode:
+        message = dcode
+        dcode = "invalid_card"
+    elif "does not support" in dcode:
+        message = dcode
+        dcode = "card_not_support"
+    elif "authentication" in message:
+        dcode = "3ds_vbv"
     return resp["status"], dcode, message, resp["time"], emoji
 
 
@@ -58,13 +58,21 @@ async def _stripe(e):
             cc, round(int(time), 2)
         )
     else:
-        msg = '**¥ Stripe/Charge/1$**'
-        msg += '\n**› Card:** `{}`'
-        msg += '\n**› Status:** {}'
-        msg += '\n**› Message:** {}'
-        msg += '\n**› Code:** {}'
-        msg += '\n**› TimeTaken:** ```{}s```'
-        msg += '\n**› Credits:** 10k'
-        msg += '\n**› Checked By:** [{}](tg://user?id={})'
-        msg = msg.format(arg, emoji, mess, code, round(int(time), 2), e.sender.first_name, e.sender_id)
+        msg = "**¥ Stripe/Charge/1$**"
+        msg += "\n**› Card:** `{}`"
+        msg += "\n**› Status:** {}"
+        msg += "\n**› Message:** {}"
+        msg += "\n**› Code:** {}"
+        msg += "\n**› TimeTaken:** ```{}s```"
+        msg += "\n**› Credits:** 10k"
+        msg += "\n**› Checked By:** [{}](tg://user?id={})"
+        msg = msg.format(
+            arg,
+            emoji,
+            mess,
+            code,
+            round(int(time), 2),
+            e.sender.first_name,
+            e.sender_id,
+        )
     await message.edit(msg)
