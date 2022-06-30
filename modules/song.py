9bc5988200373540a62ad28b445b265b27050ff8
Amarnath C <amarnathcharichilil@gmail.com>
2022-06-30 22:28:47 +0530

    Create song.py

---
+++ modules/song.py
@@ -0,0 +1,17 @@
+from requests import get
+from ._handler import newMsg
+import io
+
+HOST = 'https://api.roseloverx.tk/youtube/download'
+
+@newMsg(pattern="song")
+async def _song(e):
+ try:
+   q = e.text.split(None, maxsplit=1)[1]
+ except IndexError:
+   return await e.reply('song query missing.')
+ params = {'query': q, 'download': 'true'}
+ r = get(HOST, params=params)
+ with io.BytesIO(r.content) as file:
+      file.name = r.headers.get("file-name") or "song.mp3"
+      await e.respond(file=file)
