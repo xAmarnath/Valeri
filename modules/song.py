e28f898655ef898945161f2fdb820a1f5f175c09
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-06-30 16:59:08 +0000

    RoseLoverX: auto fixes

--- modules/song.py
+++ modules/song.py
@@ -1,17 +1,20 @@
-from requests import get
-from ._handler import newMsg
 import io
 
-HOST = 'https://api.roseloverx.tk/youtube/download'
+from requests import get
+
+from ._handler import newMsg
+
+HOST = "https://api.roseloverx.tk/youtube/download"
+
 
 @newMsg(pattern="song")
 async def _song(e):
- try:
-   q = e.text.split(None, maxsplit=1)[1]
- except IndexError:
-   return await e.reply('song query missing.')
- params = {'query': q, 'download': 'true'}
- r = get(HOST, params=params)
- with io.BytesIO(r.content) as file:
-      file.name = r.headers.get("file-name") or "song.mp3"
-      await e.respond(file=file)
+    try:
+        q = e.text.split(None, maxsplit=1)[1]
+    except IndexError:
+        return await e.reply("song query missing.")
+    params = {"query": q, "download": "true"}
+    r = get(HOST, params=params)
+    with io.BytesIO(r.content) as file:
+        file.name = r.headers.get("file-name") or "song.mp3"
+        await e.respond(file=file)
