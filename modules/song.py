95c61958ea7e1419a8e2219fb106a60346c4318c
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-07-01 04:13:42 +0000

    RoseLoverX: auto fixes

--- modules/song.py
+++ modules/song.py
@@ -19,37 +19,39 @@ async def _song(message):
     song = search_song(query=query)
     if song is None:
         return await message.reply("song not found.")
-    params = {"id": song['id'], "download": "true"}
+    params = {"id": song["id"], "download": "true"}
     response = get(HOST + "/youtube/download", params=params)
     with io.BytesIO(response.content) as file:
         file.name = response.headers.get("file-name") or "song.mp3"
         async with message.client.action(message.chat_id, "audio"):
             await message.respond(
                 file=file,
-                attributes=[types.DocumentAttributeAudio(
-                    duration=convert_duration(song["duration"]),
-                    title=song["title"],
-                    performer=song["channel"],
-                )],
+                attributes=[
+                    types.DocumentAttributeAudio(
+                        duration=convert_duration(song["duration"]),
+                        title=song["title"],
+                        performer=song["channel"],
+                    )
+                ],
             )
 
 
 def search_song(query):
-    '''
+    """
     Search for a song on youtube and return the first result.
-    '''
+    """
     params = {"q": query}
     request = get(HOST + "/youtube/search", params=params)
-    request = request.json() if request.status_code == 200 else {'data': []}
-    if len(request['data']) == 0:
+    request = request.json() if request.status_code == 200 else {"data": []}
+    if len(request["data"]) == 0:
         return None
     return request["data"][0]
 
 
 def convert_duration(duration):
-    '''
+    """
     Converts a duration in the format of HH:MM:SS to seconds.
-    '''
+    """
     duration = duration.split(":")
     if len(duration) == 3:
         return int(duration[0]) * 60 * 60 + int(duration[1]) * 60 + int(duration[2])
