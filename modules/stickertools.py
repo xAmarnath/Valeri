f8a6b410b359c017e0d6ff6472af4f85b6f0a9ee
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-07-03 03:16:44 +0000

    RoseLoverX: auto fixes

--- modules/stickertools.py
+++ modules/stickertools.py
@@ -1,46 +1,59 @@
+import asyncio
 from os import remove, rename
 
-from ._handler import newMsg
-from requests import post , get
-import asyncio
 from PIL import Image
+from requests import get, post
+
+from ._handler import newMsg
+
 
 async def run_cmd(cmd):
- proc = await asyncio.create_subprocess_shell(
+    proc = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
- o, e = await proc.communicate()
- return str(o), str(e)
+    o, e = await proc.communicate()
+    return str(o), str(e)
+
 
 def color_image(path):
-    with open(path, 'rb') as file:
-     r = post('https://api.deepai.org/api/colorizer', files={"image": file}, headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'})
-     with open('color-' + path, 'wb') as file:
-      file.write(get(r.json()['output_url']))
-    return 'color-' + path
+    with open(path, "rb") as file:
+        r = post(
+            "https://api.deepai.org/api/colorizer",
+            files={"image": file},
+            headers={"api-key": "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"},
+        )
+        with open("color-" + path, "wb") as file:
+            file.write(get(r.json()["output_url"]))
+    return "color-" + path
+
 
 def similarize_image(image):
     image1 = Image.open(image)
-    image2 = Image.open('color-' + image)
+    image2 = Image.open("color-" + image)
     image2 = image2.resize((image1.size[0], image1.size[1]))
-    image2.save('color-' + image)
+    image2.save("color-" + image)
+
 
 FFMPEG_COMMAND = 'ffmpeg -loop 1 -framerate 30 -t 0.16 -i {}  -loop 1 -framerate 30 -t 0.16 -i {} -loop 1 -framerate 30 -t 0.16 -i {} -loop 1 -framerate 30 -t 0.16 -i {} -filter_complex "[0][1][2][3]concat=n=4:v=1:a=0[v1],[v1]loop=20:32767:0" {}'
 
-@newMsg(pattern='animate')
+
+@newMsg(pattern="animate")
 async def _animate(msg):
- if not msg.reply_to:
-    return await e.reply("Reply to sticker/photo to animate it")
- r = await msg.get_reply_message()
- mg = await msg.reply('`Processing..`')
- if not any([r.photo, r.sticker]):
-    return await e.reply('nil')
- f = await r.download_media()
- color_f = color_image(f)
- similarize_image(f)
- await run_cmd(FFMPEG_COMMAND.format(f, color_f, f, color_f, '{}-anim.mp4'.format(msg.id)))
- await e.respond(file='{}-anim.mp4'.format(msg.id))
- await mg.delete()
+    if not msg.reply_to:
+        return await e.reply("Reply to sticker/photo to animate it")
+    r = await msg.get_reply_message()
+    mg = await msg.reply("`Processing..`")
+    if not any([r.photo, r.sticker]):
+        return await e.reply("nil")
+    f = await r.download_media()
+    color_f = color_image(f)
+    similarize_image(f)
+    await run_cmd(
+        FFMPEG_COMMAND.format(f, color_f, f, color_f, "{}-anim.mp4".format(msg.id))
+    )
+    await e.respond(file="{}-anim.mp4".format(msg.id))
+    await mg.delete()
+
 
 @newMsg(pattern="(stoi|itos)")
 async def _stoi(message):
