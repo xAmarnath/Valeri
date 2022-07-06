ca49ecf125f909dc4b74237abc166da94d018ef2
Amarnath C <amarnathcharichilil@gmail.com>
2022-07-06 20:08:05 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -25,8 +25,7 @@ async def _song(message):
     with io.BytesIO(response.content) as file:
         with io.BytesIO(get(song["thumbnail"]).content) as thumb:
             thumb.name = "thumbnail.jpg"
-            image = Image.open(thumb)
-            thumb = image.resize((320, 320))  # Resize the thumbnail
+            # TODO Resize the thumbnail
             file.name = response.headers.get("file-name") or "song.mp3"
             async with message.client.action(message.chat_id, "audio"):
                 await message.respond(
