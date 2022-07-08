4ff5cf2541080edcb48956a53f743ddbb3c7c8be
Amarnathcjd <amarnathcharichilil@gmail.com>
2022-07-08 14:40:39 +0530

    ok

--- modules/_transfers.py
+++ modules/_transfers.py
@@ -1,8 +1,3 @@
-"""
-> Based on parallel_file_transfer.py from mautrix-telegram, with permission to distribute under the MIT license
-> Copyright (C) 2019 Tulir Asokan - https://github.com/tulir/mautrix-telegram
-"""
-from .FastTelethon import download_file, upload_file
 import time
 import sys
 import pathlib
