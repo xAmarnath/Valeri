74c1e5c0c496b4d1f4b741770cef905f34af742f
Amarnath C <amarnathcharichilil@gmail.com>
2022-07-13 08:20:47 +0530

    Update torr.py

--- modules/torr.py
+++ modules/torr.py
@@ -20,21 +20,7 @@ def aria_start():
         "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt"
     ).text.replace("\n\n", ",")
     trackers = f"[{trackers_list}]"
-    cmd = f"aria2c \
-          --enable-rpc \
-          --rpc-listen-all=false \
-          --rpc-listen-port=6800 \
-          --max-connection-per-server=100 \
-          --rpc-max-request-size=1024M \
-          --check-certificate=false \
-          --follow-torrent=mem \
-          --seed-time=600 \
-          --min-split-size=10M \
-          --follow-torrent=mem \
-          --split=10 \
-          --bt-tracker={trackers} \
-          --daemon=true \
-          --allow-overwrite=true"
+    cmd = f"aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --max-connection-per-server=100 --rpc-max-request-size=1024M --check-certificate=false --follow-torrent=mem --seed-time=600 --min-split-size=10M --follow-torrent=mem --split=10 --bt-tracker={trackers} --daemon=true --allow-overwrite=true"
     subprocess_run(cmd)
     aria2 = aria2p.API(aria2p.Client(host="http://localhost", port=6800, secret=""))
     return aria2
