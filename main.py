6ec2e840b5efce49227667cefe4ad6fedc8c7753
Amarnathcjd <amarnathcharichilil@gmail.com>
2022-07-13 11:05:33 +0530

    op

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import http.server
-import socketserver
-from os import getenv
-
 from modules._config import TOKEN, bot
 from modules._helpers import __load_modules
 
-PORT = int(getenv("PORT", "8080"))
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
+
 
 bot.start(bot_token=TOKEN)
 
---
+++ web.py
@@ -0,0 +1,8 @@
+import http.server
+import socketserver
+from os import getenv
+
+PORT = int(getenv("PORT", "8080"))
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
