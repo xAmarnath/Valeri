fab65f55f71ab8340fac0b58c55cdec876c5ca6c
Amarnathcjd <amarnathcharichilil@gmail.com>
2022-07-13 11:00:09 +0530

    oki

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from modules._config import TOKEN, bot
 from modules._helpers import __load_modules
+import http.server
+import socketserver
+from os import getenv
+
+PORT = getenv("PORT", 8080)
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
 
 bot.start(bot_token=TOKEN)
 
