227961002bffcd465c5e0ba0895cffe45a4f5e2f
Amarnathcjd <amarnathcharichilil@gmail.com>
2022-07-13 11:07:44 +0530

    hh

--- web.py
+++ web.py
@@ -6,3 +6,4 @@ PORT = int(getenv("PORT", "8080"))
 Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
+httpd.serve_forever()
