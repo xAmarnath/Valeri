e27ac0d6b42c62df48ed79673b89b1f85cc01238
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-07-13 02:53:50 +0000

    RoseLoverX: auto fixes

--- modules/torr.py
+++ modules/torr.py
@@ -19,7 +19,7 @@ def aria_start():
     trackers_list = get(
         "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt"
     ).text.replace("\n\n", ",")
-    trackers = f"[{trackers_list}]"
+    f"[{trackers_list}]"
     cmd = "aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --max-connection-per-server=100 --rpc-max-request-size=1024M --check-certificate=false --follow-torrent=mem --seed-time=600 --min-split-size=10M --follow-torrent=mem --split=10 --daemon=true --allow-overwrite=true"
     subprocess_run(cmd)
     aria2 = aria2p.API(aria2p.Client(host="http://localhost", port=6800, secret=""))
