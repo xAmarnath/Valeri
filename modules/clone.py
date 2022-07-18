134574df7870794d9503a2925816762d38bda8fd
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-07-18 14:04:21 +0000

    RoseLoverX: auto fixes

--- modules/clone.py
+++ modules/clone.py
@@ -1,32 +1,36 @@
-from ._config import API_KEY, API_HASH
+from telethon import TelegramClient as client
+
+from ._config import API_HASH, API_KEY
 from ._handlers import newMsg
 from ._helpers import get_text_content
-from telethon import TelegramClient as client
 
 clients = []
 
+
 async def start_(msg):
-     return await msg.reply("Hello Im, botID-{}")
+    return await msg.reply("Hello Im, botID-{}")
+
 
 def load_handlers(bot):
     bot.add_event_handler(start_, events.NewMessage(pattern="^[!/?]start$"))
 
+
 async def addBot(token):
- botID = token.split(":")[0]
- tgClient = client(botID+'-0', API_KEY, API_HASH)
- clients.append(tgClient)
- try:
-  await tgClient.start(bot_token=token)
- except Exception as d:
-  return str(d)
- load_handlers(tgClient)
- return ''
+    botID = token.split(":")[0]
+    tgClient = client(botID + "-0", API_KEY, API_HASH)
+    clients.append(tgClient)
+    try:
+        await tgClient.start(bot_token=token)
+    except Exception as d:
+        return str(d)
+    load_handlers(tgClient)
+    return ""
+
 
 @newMsg(pattern="addbot")
 async def addbt(e):
- tok = await get_text_content(e)
- add = await addBot(tok)
- if add!= '':
-    return await e.reply(add)
- return await e.reply('Sucessfully added bot.')
- 
+    tok = await get_text_content(e)
+    add = await addBot(tok)
+    if add != "":
+        return await e.reply(add)
+    return await e.reply("Sucessfully added bot.")
