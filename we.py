5071783722ab679aa689f781c8503e1766f05779
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-07-19 01:36:02 +0000

    RoseLoverX: auto fixes

--- we.py
+++ we.py
@@ -1,10 +1,15 @@
 from os import getenv
+
 from aiohttp.web import Application, run_app
+
 app = Application()
-app.router.add_static('/files/', './static')
+app.router.add_static("/files/", "./static")
+
+
 async def root_handler(request):
-    return aiohttp.web.HTTPFound('https://google.com')
-app.router.add_route('*', '/', root_handler)
+    return aiohttp.web.HTTPFound("https://google.com")
+
+
+app.router.add_route("*", "/", root_handler)
 
 run_app(app, port=int(getenv("PORT")))
-
