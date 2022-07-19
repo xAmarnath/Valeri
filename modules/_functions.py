da8e9b67b5acf7769b7cee974a99d410f963b101
Amarnath C <amarnathcharichilil@gmail.com>
2022-07-19 19:43:59 +0530

    Update _functions.py

--- modules/_functions.py
+++ modules/_functions.py
@@ -330,7 +330,6 @@ def netflix_login(combos):
             "https://www.netflix.com/login",
             headers=headers,
             data=data,
-            proxies=random.choice(self.proxies),
         )
         cookie = dict(
             flwssn=client.get(
@@ -361,7 +360,6 @@ def netflix_login(combos):
                     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
                 },
                 cookies=cookie,
-                proxies=random.choice(self.proxies),
                 timeout=10,
             ).text
             plan = info.split('data-uia="plan-label"><b>')[1].split("</b>")[0]
