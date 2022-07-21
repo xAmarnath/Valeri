f915ce3d43a4c73530bcae1d201858114d103af7
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-07-21 13:39:00 +0000

    RoseLoverX: auto fixes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from modules._config import TOKEN, bot, OWNER_ID
+from modules._config import OWNER_ID, TOKEN, bot
 from modules._helpers import __load_modules
 
 bot.start(bot_token=TOKEN)
 if not OWNER_ID == 1833850637:
- exit(69) # huehue
+    exit(69)  # huehue
 
 __load_modules()
 
