acbd26e66bcb1ce5045b29b78ee1690ceed653d8
Amarnath C <amarnathcharichilil@gmail.com>
2022-07-28 22:11:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from modules._config import OWNER_ID, TOKEN, bot
 from modules._helpers import load_modules
 
 bot.start(bot_token=TOKEN)
-if not OWNER_ID == 1833850637:
-    exit(69)  # huehue
 
 load_modules()
 
