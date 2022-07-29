5a77cc3ce381f23c648a090c33c20ce5f159668e
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-07-29 11:59:40 +0000

    RoseLoverX: auto fixes

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import requests
 
 headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 }
 
-response = requests.get('https://hackthebox.store/cart/30942215241867:1', headers=headers)
+response = requests.get(
+    "https://hackthebox.store/cart/30942215241867:1", headers=headers
+)
 
 print(response.url)
-#req1
+# req1
