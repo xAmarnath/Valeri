b6ed76abe0f0365d36eb8ec878ed82d7d59115af
Amarnathcjd <amarnathcharichilil@gmail.com>
2022-08-02 15:38:01 +0530

    do

--- web.py
+++ web.py
@@ -1,15 +1,3 @@
-from requests import post
+import requests
 
-r = post(
-    "https://voucherpub.com/wp-admin/admin-ajax.php",
-    data={
-        "attribute_package": "12 Months",
-        "quantity": "1",
-        "add-to-cart": "1514",
-        "product_id": "1514",
-        "variation_id": "1517",
-        "action": "basel_ajax_add_to_cart",
-    },
-)
 
-print(r.headers)
