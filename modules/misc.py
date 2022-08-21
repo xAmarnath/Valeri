d150c81ae98f67036d50e16782b867ec58579093
Amarnathcjd <amarnathcharichilil@gmail.com>
2022-08-21 17:59:31 +0530

    kij

--- modules/misc.py
+++ modules/misc.py
@@ -659,5 +659,6 @@ async def stream_audio(msg):
             btn = []
     if len(btn) == 1:
         buttons.append([btn[0]])
+    buttons.append([Button.web("Search on Google", "https://google.com/search?q=" + query)])
     MSG = "Found {} streams, For **{}**\nchoose one:\n".format(len(urls), query)
     await msg.reply(MSG, buttons=buttons)
