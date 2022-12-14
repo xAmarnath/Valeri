315eb7b38bd7d9d7a8cf8fa46b38bd86f1a10923
Amarnath C <amarnathcharichilil@gmail.com>
2022-12-14 21:28:04 +0530

    Create web.py

---
+++ web.py
@@ -0,0 +1,9 @@
+from functools import partial
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
+import os
+
+PORT = os.getenv("PORT", "8080)
+
+with TCPServer(("", int(PORT)), partial(SimpleHTTPRequestHandler, directory=".")) as httpd:
+    httpd.serve_forever()
