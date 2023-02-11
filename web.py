8fd0b101435f50916f80d60e2964eb03eef618b4
Amarnath C <amarnathcharichilil@gmail.com>
2023-02-12 02:57:31 +0530

    Delete web.py

--- web.py
+++
@@ -1,9 +0,0 @@
-from functools import partial
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
-import os
-
-PORT = os.getenv("PORT", "8080")
-
-with TCPServer(("", int(PORT)), partial(SimpleHTTPRequestHandler, directory=".")) as httpd:
-    httpd.serve_forever()
