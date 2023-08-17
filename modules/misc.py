c77a84f06c66ed72b9ffc7409bcbc9697682c4cb
Amarnath C <amarnathcharichilil@gmail.com>
2023-08-18 02:37:38 +0530

    Update misc.py

--- modules/misc.py
+++ modules/misc.py
@@ -732,7 +732,7 @@ async def _ajce(e):
     i = 0
     for student in result:
         for v in b:
-            if v.text == student[0]:
+            if v[0].text == student[0]:
                 continue
         b.append([Button.inline(f"{student[0]}", "dep_" + str(student[0]+"_"+student[1]))])
         i+=1
