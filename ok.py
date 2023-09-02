e7511d3dbb3251f3d9281d1199fac3a789e4b0f7
AmarnathCJD <amarnathcharichilil@gmail.com>
2023-09-02 21:26:34 +0530

    Casual Update, Cleanup

--- ok.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-   return 'Hello, World!'
-
-os.system("pip3 install -r requirements.txt && python3 main.py")
