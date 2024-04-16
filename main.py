a6065a395cfbb7e930c9cbdb7b81cd0af4232167
GithubAI <amarnathcharichilil@gmail.com>
2024-04-17 02:08:26 +0530

    remove ktu result cheker

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from modules._config import TOKEN, bot
 from modules._helpers import load_modules
-from modules.ktu import fetch_and_check_results
 
 bot.start(bot_token=TOKEN)
 
 load_modules()
-
-import asyncio
-
-asyncio.ensure_future(fetch_and_check_results())
 
 bot.run_until_disconnected()
